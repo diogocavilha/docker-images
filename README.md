e3839b66c382ddcbe132cf1b6aa2a6d88010376d
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2017-07-19 11:41:00 -0300

    Add script for installing docker and docker-compose

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Custom docker images
+
+# Don't you have docker installed yet?
+
+Running the following command you'll get docker and docker-compose installed on your machine.
+
+```sh
+sudo curl -sS https://raw.githubusercontent.com/diogocavilha/docker-images/docker.sh | sh
+```
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Author: Diogo Alexsander Cavilha <diogocavilha@gmail.com>
+# Date: 04.18.2017
+#
+# This script installs docker and docker-compose automatically.
+
+install_docker() {
+    wget -qO- https://get.docker.com/ | sh
+    sudo docker -v
+}
+
+install_dockercompose() {
+    local url_download="https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)"
+    curl -L $url_download > /usr/local/bin/docker-compose && \
+    chmod +x /usr/local/bin/docker-compose
+    docker-compose -v
+}
+
+docker_adduser_info() {
+    echo -e "\nRun the following command to be able to run docker as a normal user:"
+    echo -e "\nsudo usermod -aG docker <user>\n"
+}
+
+install_docker
+install_dockercompose
+docker_adduser_info
